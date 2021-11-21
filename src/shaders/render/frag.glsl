59e3922c2630e8940934859594a30be6e0ef84a6
mosure <mosure@wisc.edu>
2021-11-20 23:51:47 -0600

    initial work

--- src/shaders/render/frag.glsl
+++ src/shaders/render/frag.glsl
@@ -2,15 +2,91 @@
 precision highp float;
 
 #define PI                  3.14159265359
-#define RESET_TIME_SECONDS  2.0
+#define ORIGIN              2.0 * PI + PI / 2.0
+#define RADIUS              0.25
+#define DIFFUSION           0.5
+#define PLATE_COLOR         vec3(0.0, 0.0, 0.0) / 255.0
+#define HOUR_INTENSITY      0.9
+#define HOUR_THETA          1.0 / 6.0 / 2.0
+#define MINUTE_INTENSITY    0.7
+#define MINUTE_THETA        1.0 / 6.0 / 2.0
+#define SECOND_INTENSITY    0.5
+#define SECOND_THETA        1.0 / 6.0 / 2.0
 
 uniform vec2        u_resolution;
-uniform sampler2D   u_state;
 uniform float       u_time;
+
+uniform float       u_hour;
+uniform float       u_minute;
+uniform float       u_second;
+uniform int         u_millisecond;
 
 out vec4 outColor;
 
 
+// https://stackoverflow.com/questions/26070410/robust-atany-x-on-glsl-for-converting-xy-coordinate-to-angle
+float atan2(in float y, in float x) {
+    return x == 0.0 ? sign(y) * PI / 2.0 : atan(y, x);
+}
+
+float invert(in float x) {
+    return -min(x, 1.0) + 1.0;
+}
+
+float circle(in vec2 _st, in float _radius){
+    vec2 dist = _st-vec2(0.5);
+    return 1.-smoothstep(_radius-(_radius*0.01),
+                            _radius+(_radius*0.01),
+                            dot(dist,dist)*4.0);
+}
+
+float hour_intensity(in float theta) {
+    float hour_pos = u_hour / 12.0;
+
+    float region = smoothstep(hour_pos - HOUR_THETA, hour_pos, theta) *
+                   smoothstep(hour_pos + HOUR_THETA, hour_pos, theta);
+
+    return 1.0 - region * HOUR_INTENSITY;
+}
+
+float minute_intensity(in float theta) {
+    float minute_pos = u_minute / 60.0;
+
+    float region = smoothstep(minute_pos - MINUTE_THETA, minute_pos, theta) *
+                   smoothstep(minute_pos + MINUTE_THETA, minute_pos, theta);
+
+    return 1.0 - region * MINUTE_INTENSITY;
+}
+
+float second_intensity(in float theta) {
+    float second_pos = u_second / 60.0;
+
+    float region = smoothstep(second_pos - SECOND_THETA, second_pos, theta) *
+                   smoothstep(second_pos + SECOND_THETA, second_pos, theta);
+
+    return 1.0 - region * SECOND_INTENSITY;
+}
+
 void main() {
-    outColor = texture(u_state, gl_FragCoord.xy / u_resolution);
+    vec2 st = gl_FragCoord.xy / u_resolution.xy;
+    vec2 center = vec2(0.5);
+    vec2 delta = st - center;
+    float dist = distance(st, center);
+
+    float theta = fract((ORIGIN - atan2(delta.y, delta.x)) / (2.0 * PI));
+
+    float tan_dist = dist - (RADIUS - DIFFUSION * RADIUS);
+
+    float grad = invert(tan_dist / DIFFUSION);
+    float intensity = grad * grad * grad * grad * hour_intensity(theta) * minute_intensity(theta) * second_intensity(theta);
+
+    float mask = circle(st, RADIUS);
+    float inv_mask = 1.0 - mask;
+
+    vec3 light_color = vec3(1.0);
+
+    vec3 plate = mask * PLATE_COLOR + intensity * inv_mask;
+
+    // outColor = vec4(st.x, st.y, sin(float(u_millisecond) / 1000.0), 1.0);
+    outColor = vec4(plate, 1.0);
 }
--- src/stages.rs
+++ src/stages.rs
@@ -1,11 +1,12 @@
 use std::rc::Rc;
+use chrono::{DateTime, Timelike, NaiveDateTime, Utc};
+
+use js_sys::Date;
 
 use web_sys::{
     WebGl2RenderingContext,
-    WebGlFramebuffer,
     WebGlProgram,
     WebGlShader,
-    WebGlTexture,
 };
 
 use super::pipeline::{AppContext, Stage};
@@ -19,24 +20,16 @@ pub struct DrawStage {
     vertices: [f32; 18],
 }
 
-pub struct ComputeStage {
-    gl: Rc<WebGl2RenderingContext>,
-    ctx: Rc<AppContext>,
-    program: Rc<WebGlProgram>,
-    framebuffer: WebGlFramebuffer,
-    transfer_buffer: WebGlFramebuffer,
-    pub in_state: WebGlTexture,
-    pub out_state: WebGlTexture,
-    draw_stage: DrawStage,
-    pub frame: u32,
-}
-
 pub struct RenderStage {
     gl: Rc<WebGl2RenderingContext>,
     ctx: Rc<AppContext>,
     program: Rc<WebGlProgram>,
-    compute_stage: ComputeStage,
     draw_stage: DrawStage,
+    pub hour: f32,
+    pub minute: f32,
+    pub second: f32,
+    pub millisecond: u32,
+    pub time_offset: f64,
 }
 
 impl DrawStage {
@@ -114,241 +107,6 @@ impl Stage for DrawStage {
     }
 }
 
-impl ComputeStage {
-    pub fn new(
-        gl: Rc<WebGl2RenderingContext>,
-        ctx: Rc<AppContext>,
-    ) -> ComputeStage {
-        let vert_shader = compile_shader(
-            &gl,
-            WebGl2RenderingContext::VERTEX_SHADER,
-            include_str!("shaders/state/vert.glsl"),
-        ).expect("expect vertex shader");
-
-        let frag_shader = compile_shader(
-            &gl,
-            WebGl2RenderingContext::FRAGMENT_SHADER,
-            include_str!("shaders/state/frag.glsl"),
-        ).expect("expect frag shader");
-
-        let program = Rc::new(
-            link_program(
-                &gl,
-                &vert_shader,
-                &frag_shader
-            ).expect("expect linked program")
-        );
-
-        let framebuffer = gl.create_framebuffer().expect("should have a framebuffer");
-        let transfer_buffer = gl.create_framebuffer().expect("should have a framebuffer");
-        let in_state = gl.create_texture().expect("should have a texture");
-        let out_state = gl.create_texture().expect("should have a texture");
-
-        let draw_stage = DrawStage::new(
-            Rc::clone(&gl),
-            Rc::clone(&program),
-        );
-
-        let mut stage = ComputeStage {
-            gl: gl,
-            ctx: ctx,
-            program: program,
-            framebuffer: framebuffer,
-            transfer_buffer: transfer_buffer,
-            in_state: in_state,
-            out_state: out_state,
-            draw_stage: draw_stage,
-            frame: 0,
-        };
-        stage.init();
-
-        return stage;
-    }
-
-    fn init(&mut self) {
-        self.gl.use_program(Some(&self.program));
-
-        self.gl.viewport(0, 0, self.ctx.width as i32, self.ctx.height as i32);
-
-        {
-            self.gl.bind_framebuffer(
-                WebGl2RenderingContext::FRAMEBUFFER,
-                Some(&self.transfer_buffer)
-            );
-
-            self.gl.active_texture(
-                WebGl2RenderingContext::TEXTURE0,
-            );
-
-            self.gl.bind_texture(
-                WebGl2RenderingContext::TEXTURE_2D,
-                Some(&self.in_state)
-            );
-
-            self.gl.tex_parameteri(
-                WebGl2RenderingContext::TEXTURE_2D,
-                WebGl2RenderingContext::TEXTURE_MAG_FILTER,
-                WebGl2RenderingContext::NEAREST as i32
-            );
-            self.gl.tex_parameteri(
-                WebGl2RenderingContext::TEXTURE_2D,
-                WebGl2RenderingContext::TEXTURE_MIN_FILTER,
-                WebGl2RenderingContext::NEAREST as i32
-            );
-
-            self.gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
-                WebGl2RenderingContext::TEXTURE_2D,
-                0,
-                WebGl2RenderingContext::RGBA as i32,
-                self.ctx.width as i32,
-                self.ctx.height as i32,
-                0,
-                WebGl2RenderingContext::RGBA as u32,
-                WebGl2RenderingContext::UNSIGNED_BYTE,
-                None,
-            ).expect("expect tex image 2d result");
-
-            self.gl.framebuffer_texture_2d(
-                WebGl2RenderingContext::FRAMEBUFFER,
-                WebGl2RenderingContext::COLOR_ATTACHMENT0,
-                WebGl2RenderingContext::TEXTURE_2D,
-                Some(&self.in_state),
-                0,
-            );
-        }
-
-        {
-            self.gl.bind_framebuffer(
-                WebGl2RenderingContext::FRAMEBUFFER,
-                Some(&self.framebuffer)
-            );
-
-            self.gl.active_texture(
-                WebGl2RenderingContext::TEXTURE1,
-            );
-
-            self.gl.bind_texture(
-                WebGl2RenderingContext::TEXTURE_2D,
-                Some(&self.out_state)
-            );
-
-            self.gl.tex_parameteri(
-                WebGl2RenderingContext::TEXTURE_2D,
-                WebGl2RenderingContext::TEXTURE_MAG_FILTER,
-                WebGl2RenderingContext::NEAREST as i32
-            );
-            self.gl.tex_parameteri(
-                WebGl2RenderingContext::TEXTURE_2D,
-                WebGl2RenderingContext::TEXTURE_MIN_FILTER,
-                WebGl2RenderingContext::NEAREST as i32
-            );
-
-            self.gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
-                WebGl2RenderingContext::TEXTURE_2D,
-                0,
-                WebGl2RenderingContext::RGBA as i32,
-                self.ctx.width as i32,
-                self.ctx.height as i32,
-                0,
-                WebGl2RenderingContext::RGBA as u32,
-                WebGl2RenderingContext::UNSIGNED_BYTE,
-                None,
-            ).expect("expect tex image 2d result");
-
-            self.gl.framebuffer_texture_2d(
-                WebGl2RenderingContext::FRAMEBUFFER,
-                WebGl2RenderingContext::COLOR_ATTACHMENT0,
-                WebGl2RenderingContext::TEXTURE_2D,
-                Some(&self.out_state),
-                0,
-            );
-        }
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::FRAMEBUFFER,
-            None
-        );
-    }
-
-    pub fn transfer_texture(&mut self) {
-        self.gl.finish();
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::READ_FRAMEBUFFER,
-            Some(&self.framebuffer)
-        );
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::DRAW_FRAMEBUFFER,
-            Some(&self.transfer_buffer)
-        );
-
-        self.gl.blit_framebuffer(
-            0,
-            0,
-            self.ctx.width as i32,
-            self.ctx.height as i32,
-            0,
-            0,
-            self.ctx.width as i32,
-            self.ctx.height as i32,
-            WebGl2RenderingContext::COLOR_BUFFER_BIT,
-            WebGl2RenderingContext::NEAREST,
-        );
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::READ_FRAMEBUFFER,
-            None
-        );
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::DRAW_FRAMEBUFFER,
-            None
-        );
-    }
-}
-
-impl Stage for ComputeStage {
-    fn render(&mut self) {
-        self.gl.use_program(Some(&self.program));
-
-        self.gl.active_texture(
-            WebGl2RenderingContext::TEXTURE0,
-        );
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::FRAMEBUFFER,
-            Some(&self.framebuffer)
-        );
-
-        let u_frame = self.gl.get_uniform_location(&self.program, "u_frame");
-        let u_resolution = self.gl.get_uniform_location(&self.program, "u_resolution");
-        let u_time = self.gl.get_uniform_location(&self.program, "u_time");
-
-        self.gl.uniform1i(u_frame.as_ref(), self.frame as i32);
-
-        let mut u_res_val = [self.ctx.width as f32, self.ctx.height as f32];
-        self.gl.uniform2fv_with_f32_array(u_resolution.as_ref(), &mut u_res_val);
-
-        let start = perf_to_system(performance().now());
-        let since_the_epoch = start
-            .duration_since(self.ctx.boot_time)
-            .expect("Time went backwards");
-        self.gl.uniform1f(u_time.as_ref(), since_the_epoch.as_secs_f32());
-
-        self.draw_stage.render();
-
-        self.gl.bind_framebuffer(
-            WebGl2RenderingContext::FRAMEBUFFER,
-            None
-        );
-
-        self.transfer_texture();
-
-        self.frame += 1;
-    }
-}
-
 impl RenderStage {
     pub fn new(
         gl: Rc<WebGl2RenderingContext>,
@@ -374,11 +132,6 @@ impl RenderStage {
             ).expect("expect linked program")
         );
 
-        let compute_stage = ComputeStage::new(
-            Rc::clone(&gl),
-            Rc::clone(&ctx),
-        );
-
         let draw_stage = DrawStage::new(
             Rc::clone(&gl),
             Rc::clone(&program),
@@ -388,8 +141,12 @@ impl RenderStage {
             gl: gl,
             ctx: ctx,
             program: program,
-            compute_stage: compute_stage,
             draw_stage: draw_stage,
+            hour: 0.0,
+            minute: 0.0,
+            second: 0.0,
+            millisecond: 0,
+            time_offset: Date::now(),
         };
         stage.init();
 
@@ -405,7 +162,19 @@ impl RenderStage {
 
 impl Stage for RenderStage {
     fn render(&mut self) {
-        self.compute_stage.render();
+        let start = perf_to_system(performance().now());
+        let since_the_boot = start
+            .duration_since(self.ctx.boot_time)
+            .expect("Time went backwards");
+
+        self.millisecond = since_the_boot.as_millis() as u32 % 1000;
+
+        let central_offset = 6;
+        let dt = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(((self.time_offset / 1000.0) as u64 + since_the_boot.as_secs()) as i64, 0), Utc);
+
+        self.hour = dt.hour() as f32 + central_offset as f32 + dt.minute() as f32 / 60.0 + dt.second() as f32 / 60.0 / 60.0 + self.millisecond as f32 / 60.0 / 60.0 / 1000.0;
+        self.minute = dt.minute() as f32 + dt.second() as f32 / 60.0 + self.millisecond as f32 / 60.0 / 1000.0;
+        self.second = dt.second() as f32 + self.millisecond as f32 / 1000.0;
 
         self.gl.use_program(Some(&self.program));
 
@@ -413,18 +182,23 @@ impl Stage for RenderStage {
             WebGl2RenderingContext::TEXTURE0,
         );
 
+        let u_hour = self.gl.get_uniform_location(&self.program, "u_hour");
+        let u_minute = self.gl.get_uniform_location(&self.program, "u_minute");
+        let u_second = self.gl.get_uniform_location(&self.program, "u_second");
+        let u_millisecond = self.gl.get_uniform_location(&self.program, "u_millisecond");
+
         let u_resolution = self.gl.get_uniform_location(&self.program, "u_resolution");
         let u_time = self.gl.get_uniform_location(&self.program, "u_time");
+
+        self.gl.uniform1f(u_hour.as_ref(), self.hour as f32);
+        self.gl.uniform1f(u_minute.as_ref(), self.minute as f32);
+        self.gl.uniform1f(u_second.as_ref(), self.second as f32);
+        self.gl.uniform1i(u_millisecond.as_ref(), self.millisecond as i32);
 
         let mut u_res_val = [self.ctx.width as f32, self.ctx.height as f32];
         self.gl.uniform2fv_with_f32_array(u_resolution.as_ref(), &mut u_res_val);
 
-        let start = perf_to_system(performance().now());
-        let since_the_epoch = start
-            .duration_since(self.ctx.boot_time)
-            .expect("Time went backwards");
-
-        self.gl.uniform1f(u_time.as_ref(), since_the_epoch.as_secs_f32());
+        self.gl.uniform1f(u_time.as_ref(), since_the_boot.as_secs_f32());
 
         self.draw_stage.render();
 

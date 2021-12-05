55ad4567f460aa0448dead1df56cd39a90dc2154
mosure <mosure@wisc.edu>
2021-12-05 17:54:20 -0600

    slower color change

--- rpi/effect.py
+++ rpi/effect.py
@@ -70,7 +70,7 @@ def dev_effect():
         st = x / resolution
 
         ct = run_time % 1.0
-        it = (run_time * 14 / 13) % 1.0
+        it = (run_time * 3 / 13) % 1.0
 
         r,g,b = colorsys.hsv_to_rgb(it, 0.6, 0.5)
 

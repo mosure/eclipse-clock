786ed19a143e72264b11f75fddb234194278e498
mosure <mosure@wisc.edu>
2021-12-06 01:33:00 -0600

    solid color

--- rpi/effect.py
+++ rpi/effect.py
@@ -72,7 +72,8 @@ def color_noise(segment = True):
         s = math.cos(st * 2 * math.pi)
         t = math.sin(st * 2 * math.pi)
 
-        noise = ((snoise3(s, t, run_time / 3, octaves=8) + 1) / 2 + run_time / 10) % 1
+        #hue = ((snoise3(s, t, run_time / 3, octaves=8) + 1) / 2 + run_time / 10) % 1
+        hue = (run_time / 6) % 1
 
         #ct = (run_time / 4) % 1.0
 
@@ -83,7 +84,7 @@ def color_noise(segment = True):
         else:
             intensity = 0.4
 
-        r,g,b = colorsys.hsv_to_rgb(noise, 0.5, intensity)
+        r,g,b = colorsys.hsv_to_rgb(hue, 0.5, intensity)
 
         return tuple([
             r * 255,
